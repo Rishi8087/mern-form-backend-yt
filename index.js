a4e2e2057487e553dc1ae41c2f7ac41c15dfd273
rishi singh <85188638+rishi803@users.noreply.github.com>
2023-07-14 03:39:32 +0530

    update port

--- index.js
+++ index.js
@@ -105,9 +105,9 @@ app.post("/register", async (req, res) => {
   }
 });
 
-// const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8000;
 
-app.listen(8000, () => {
+app.listen(port, () => {
   console.log(`Server listening on port 8000`);
 });
 
